7a2def365b8361f08a36204f7e5e314831cc47ff
HighCoder98 <highcoder@outlook.de>
2016-12-28 12:24:16 +0100

    Added code

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Config for API Access
+"""
+
+API_USER = "<your-username-here>"
+API_PASSWORD = "<your-password-here>"
+API_ADDRESS = "http://<your-address-here>:9981/"
