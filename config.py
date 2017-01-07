9fa925ab57ea9e110e72c7e29139719f42a507a3
HighCoder98 <highcoder@outlook.de>
2017-01-07 22:57:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ Config for API Access
 API_USER = "<your-username-here>"
 API_PASSWORD = "<your-password-here>"
 API_ADDRESS = "http://<your-address-here>:9981/"
+API_IS_SELFSIGNED = <True or False> # True for selfsigned SSL certificate
